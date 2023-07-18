eb798cc6073ddb1d16f18f69af7d25a45fccf390
scottcaruso <sccaruso94@gmail.com>
2023-07-17 22:12:34 -0600

    added feature request

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
+
 with col1:
     st.image("images/profile_photo.png", width=450)
 
@@ -14,3 +15,9 @@ with col2:
     One of my greatest passions lies in the realm of technology and software development. As an avid Python enthusiast, I thrive on the art of crafting innovative applications. Whether it's building intuitive user interfaces or implementing intricate algorithms, I immerse myself in the world of coding. The process of transforming ideas into tangible software brings me immense joy and a sense of accomplishment.
     """
     st.info(content)
+
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!"""
+
+st.write(content2)
